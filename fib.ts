6dd403288216057e2f7be72d167dab1617749bf2
zsiefkes <backwardsxmarathon@gmail.com>
2021-02-21 21:20:27 +1300

    Add inline method documentation

--- fib.ts
+++ fib.ts
@@ -1,3 +1,12 @@
+/**
+ * Returns the nth Fibonacci number. The nth Fibonacci number is defined as:
+ *  0 for n = 0;
+ *  1 for n = 1;
+ *  the sum of the two preceding Fibonacci numbers for n > 1.
+ * @param n A non-negative integer.
+ * @return The nth Fibonacci number.
+ * @throws Error: Argument must be non-negative.
+ */
 export const fibNum = (n: number): number => {
     if (n < 0) throw new Error("Argument must be non-negative");
     if (n == 0) return 0;
