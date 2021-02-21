f1e08b63dde6e28d58b628ec194be94c19f95d43
zsiefkes <backwardsxmarathon@gmail.com>
2021-02-21 21:00:53 +1300

    Pass test to throw error for negative argument

--- fib.ts
+++ fib.ts
@@ -1,4 +1,5 @@
 export const fibNum = (n: number): number => {
+    if (n < 0) throw new Error("Argument must be non-negative");
     if (n == 0) return 0;
     if (n == 1) return 1;
     return fibNum(n - 1) + fibNum(n - 2);
